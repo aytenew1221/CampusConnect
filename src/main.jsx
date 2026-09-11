8761f16af674ee150b4546b1cb966e2227d71579
aytenew <aytenew.ayele2@gmail.com>
2026-09-11 16:03:54 +0300

    feat: configure React app providers and routing

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import "./index.css";
+
+import { FavoritesProvider } from "./context/FavoritesContext";
+import { ThemeProvider } from "./context/ThemeContext";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <FavoritesProvider>
+          <App />
+        </FavoritesProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>,
+);
